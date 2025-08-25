2bc0105828095d1f0bc74ea4fb8b5b4b246f6877
Jani Piirainen <piirainen.jani98@gmail.com>
2025-08-25 11:18:49 +0300

    6.8 valmis

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,32 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
-import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteForm from './components/AnecdoteForm.jsx'
+import AnecdoteList from './components/AnecdoteList.jsx'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
-
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-
-      {/* käytetään uutta komponenttia */}
       <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
